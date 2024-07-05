586a9e7b6ce04e1ca2c4d7e4f0684e910fe824cb
King <2175616761@qq.com>
2024-07-05 15:54:22 +0800

    。

---
+++ src/test/java/com/bupt/hospitalregistrationsystem/Controler/ApiControllerTest.java
@@ -0,0 +1,30 @@
+package com.bupt.hospitalregistrationsystem.Controler;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiControllerTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void getAllUserInfoChangeReq() {
+  }
+
+  @Test
+  void executeUserInfoChangeReq() {
+  }
+
+  @Test
+  void getAttendanceInformation() {
+  }
+}
